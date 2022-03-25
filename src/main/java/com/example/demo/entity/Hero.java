b670bd84bc3b9085fa780650f158cfc18c4dfde7
Deyan <drs600@inlumine.ual.es>
2022-03-25 19:48:43 +0100

    problemita

---
+++ src/main/java/com/example/demo/entity/Hero.java
@@ -0,0 +1,73 @@
+package com.example.demo.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "heroes")
+public class Hero {
+
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO) 
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.PERSIST)
+    @OrderColumn
+    private List<Superpower> superpowers;
+   
+
+    public Hero() {
+     
+    }
+
+    public Hero(String name) {
+        this.name = name;
+    }
+
+    public Hero(String name, List<Superpower> superpowers) {
+        this.name = name;
+        this.superpowers = superpowers;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Superpower> getSuperpowers() {
+        return superpowers;
+    }
+
+    public void setSuperpowers(List<Superpower> superpowers) {
+        this.superpowers = superpowers;
+    }
+
+    @Override
+    public String toString() {
+        return "Hero [id=" + id + ", name=" + name + "superpowers=" + superpowers.toString() +"]";
+    }
+
+}
